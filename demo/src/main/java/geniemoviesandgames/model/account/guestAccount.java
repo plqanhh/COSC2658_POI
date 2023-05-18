326f1674404b394551b7ea2ed592c0b4e293ea1f
Hoang Pham <houstonpm.vn@gmail.com>
2023-05-18 09:22:55 +0700

    fxml fix

--- demo/src/main/java/geniemoviesandgames/model/account/guestAccount.java
+++ demo/src/main/java/geniemoviesandgames/model/account/guestAccount.java
@@ -2,6 +2,7 @@ package geniemoviesandgames.model.account;
 
 import java.util.ArrayList;
 
+import geniemoviesandgames.backend.promote;
 import geniemoviesandgames.model.item.item;
 
 public class guestAccount extends account{
@@ -13,4 +14,25 @@ public class guestAccount extends account{
         super(ID, name, address, phone, rentals,LevelOfServices.Guest, username, password);
     }
 
+    public void CustomerBorrow(item itemIn) {
+        if(itemBorrow <= itemBorrowAllow){
+            itemIn.borrowItem();
+            if (itemIn.getItemStock() != 0) {
+                itemIn.setItemStock(itemIn.getItemStock() - 1);
+                accountListOfRentals.add(itemIn);
+                itemBorrow++;
+            }
+        }
+
+    }
+
+    public void CustomerReturn(item itemIn) {
+        itemIn.returnItem();
+        itemIn.setItemStock(itemIn.getItemStock() + 1);
+        itemReturned++;
+        itemBorrow--;
+        if(itemReturned==itemReturnedToPromote){
+            promote.guestToRegular(this);
+        }
+    }
 }
