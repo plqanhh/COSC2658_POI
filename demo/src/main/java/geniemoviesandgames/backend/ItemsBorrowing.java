97ffc7cd1cbde0984daff2afdcf37ce1d22ff28d
Hoang Pham <houstonpm.vn@gmail.com>
2023-05-20 21:38:40 +0700

    begin edit method

--- demo/src/main/java/geniemoviesandgames/backend/ItemsBorrowing.java
+++ demo/src/main/java/geniemoviesandgames/backend/ItemsBorrowing.java
@@ -1,7 +1,5 @@
-//TODO: edit string
-package geniemoviesandgames.backend;
 
-import java.util.ArrayList;
+package geniemoviesandgames.backend;
 
 import geniemoviesandgames.model.product.item;
 import geniemoviesandgames.model.product.item.LoanType;
@@ -9,27 +7,7 @@ import geniemoviesandgames.model.user.VipAccount;
 import geniemoviesandgames.model.user.guestAccount;
 import geniemoviesandgames.model.user.regularAccount;
 
-
 public class ItemsBorrowing extends mainSystem {
-
-    public static void borrowItems(guestAccount gAccount, ArrayList<item> itemsToBorrow) {
-        if (gAccount.getItemBorrow() + itemsToBorrow.size() <= 2) {
-            for (item i : itemsToBorrow) {
-                if (i.getLoanType() == LoanType.TWO_DAY) {
-                    System.out.println("Guest account cannot borrow 2-day items.");
-                    continue;
-                }
-                if (i.getStock() > 0) {
-                    i.borrowItem();
-                    gAccount.accBorrowItem(i);
-                } else {
-                    System.out.println("Item " + i.getTitle() + " is out of stock.");
-                }
-            }
-        } else {
-            System.out.println("Guest account can only rent a maximum of 2 items at a time.");
-        }
-    }
 
     public static void borrowItems(guestAccount gAccount, item itemsToBorrow) {
         if (gAccount.getItemBorrow() + 1 <= 2) {
@@ -48,17 +26,6 @@ public class ItemsBorrowing extends mainSystem {
         }
     }
 
-    public static void borrowItems(regularAccount rAccount, ArrayList<item> itemsToBorrow) {
-        for (item i : itemsToBorrow) {
-            if (i.getStock() > 0) {
-                i.borrowItem();
-                rAccount.accBorrowItem(i);
-            } else {
-                System.out.println("Item " + i.getTitle() + " is out of stock.");
-            }
-        }
-    }
-
     public static void borrowItems(regularAccount rAccount, item itemsToBorrow) {
 
         if (itemsToBorrow.getStock() > 0) {
@@ -70,25 +37,13 @@ public class ItemsBorrowing extends mainSystem {
         }
     }
 
-    public static void borrowItems(VipAccount vipAccount, ArrayList<item> itemsToBorrow) {
-        for (item i : itemsToBorrow) {
-            if (i.getStock() > 0) {
-                    i.borrowItem();
-                    vipAccount.accBorrowItem(i);
-            } else {
-                System.out.println("Item " + i.getID() + " is out of stock.");
-            }
-        }
-    }
-
-    public static void borrowItems(VipAccount vipAccount, item itemsToBorrow){
+    public static void borrowItems(VipAccount vipAccount, item itemsToBorrow) {
 
         if (itemsToBorrow.getStock() > 0) {
             itemsToBorrow.borrowItem();
             vipAccount.accBorrowItem(itemsToBorrow);
         } else {
-        System.out.println("Item " + itemsToBorrow.getID() + " is out of stock.");
+            System.out.println("Item " + itemsToBorrow.getID() + " is out of stock.");
         }
     }
 }
-
