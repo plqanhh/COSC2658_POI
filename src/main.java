3f8280fdd0a8858df7266d32cf51bc79fb108bec
Truong Vo Thien Nhan <s3929215@rmit.edu.vn>
2024-05-04 22:25:07 +0700

    delete: unused file

--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        map2D map = new map2D();
-        map.addNew(new Place("1", "Place One", new Point(4, 7), new String[]{"Restaurant"}));
-        map.addNew(new Place("2", "Place Two", new Point(5, 5), new String[]{"Cafe"}));
-
-        // Add search functionality demonstration here
-    }
-}
